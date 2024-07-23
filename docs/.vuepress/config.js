49a389ae44e78a6fb458ec97f1de5c9d670b53a9
fjx <1300448152@qq.com>
2024-07-23 14:46:56 +0800

    4

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@ const { copyCode } = require('vuepress-plugin-copy-code2');
 
 module.exports = {
     // 站点配置
-    base: '/webpack-docs',
+    base: '/webpack-docs/',
     lang: 'zh-CN',
     head: [['link', { rel: 'icon', href: '/imgs/favicon.ico' }]],
     title: 'Web 前端之 Webpack5 教程',
